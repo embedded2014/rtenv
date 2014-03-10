f1fba79f3a38161617fbdd079e27fd522260df4d
LanKuDot <lankudot@github.com>
2014-03-10 20:39:12 +0800

    Rename unit test string buffer

--- unit_test.c
+++ unit_test.c
@@ -4,6 +4,6 @@ void unit_test()
 {
 	/* The string that carry the result of
 	 * string-testing case. */
-	char unitTestResult[20] = {0};
+	char utString[20] = {0};
 	/* Unit Test would be done here. */
 }
